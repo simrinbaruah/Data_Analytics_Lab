b654b2a35ac40fe319241f8c3f536364c78b7b4a
simrinbaruah <simrinbaruah@gmail.com>
2021-10-03 18:27:07 +0530

    Add files via upload

---
+++ lab7/assignment7_1.R
@@ -0,0 +1,21 @@
+# 1. Import any database of transactions of your choice (Groceries‘ dataset may be preferred.
+#                                                        It is predefined in R package)
+# (a) Load required R libraries : arules and arulesViz
+library(arules)
+library(arulesViz)
+
+data(Adult)
+
+# (b) Apply apriori algorithm and mine the frequent itemsets and association rules. Use
+# the default values of minimum support and minimum confidence.
+rules <- apriori(Adult)
+
+# (c) Displays the first 10 strong association rules.
+rules<-sort(rules, decreasing = TRUE, by = "support")
+inspect(rules[1:10])
+
+# (d) Plot the 20 items with highest item frequency.
+itemFrequencyPlot(Adult, topN = 20,
+                          type = 'absolute',
+                          main = 'Frequency Plot',
+                          ylab = "Item Frequency")
---
+++ lab7/assignment7_2.R
@@ -0,0 +1,16 @@
+# 2. Continue the same with minimum support of 20% and the minimum confidence of 50%
+
+library(arules)
+library(arulesViz)
+
+data(Adult)
+
+rules <- apriori(Adult, parameter = list(supp = 0.2, conf = 0.5))
+
+rules<-sort(rules, decreasing = TRUE, by = "support")
+inspect(rules[1:10]) #first 10 strong association rules
+
+itemFrequencyPlot(Adult, topN = 20,
+                  type = 'absolute',
+                  main = 'Frequency Plot',
+                  ylab = "Item Frequency")
